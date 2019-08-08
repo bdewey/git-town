46fc39ae1dc98b15ab0f3aefb98153070426a7d9
Kevin Goslar <kevin.goslar@gmail.com>
2019-08-08 16:07:12 -0400

    progress

--- text-run/command-description.js
+++ text-run/command-description.js
@@ -15,14 +15,17 @@ function getMd(activity) {
       .map(text => text.trim())
       .join("\n")
       .replace(/\n<\/?a>\n/g, " ")
-      .replace(/ \./g, ".")
-      .replace(/ \,/g, ",")
   )
 }
 
 function nodeContent(node) {
-  if (node.type === "link_open") return "<a>"
-  if (node.type === "link_close") return "</a>"
+  if (node.type === "list_item_open") {
+    return ":" + node.content
+  }
+  console.log(node.type)
+  if (node.type === "list_close") {
+    return node.content + ":"
+  }
   return node.content
 }
 
@@ -30,18 +33,50 @@ function getCliDesc(activity) {
   const command = getCommand(activity.file)
   const output = child_process.execSync(`git-town help ${command}`).toString()
   const matches = output.match(/^.*\n\n([\s\S]*)\n\nUsage:\n/m)
-  return normalize(matches[1].replace(/- /g, "\n").replace(/[0-9]\./g, "\n"))
+  const lines = matches[1].split("\n")
+  let i = 1
+  while (i < lines.length) {
+    if (lines[i].startsWith("- ")) {
+      lines[i] = lines[i].replace(/^- /, "")
+      i += 1
+      continue
+    }
+    const lineMatch = lines[i].match(/[0-9]\. /)
+    if (lineMatch) {
+      lines[i] = lines[i].replace(lineMatch[0], "")
+      i += 1
+      continue
+    }
+    if (isTextLine(lines[i])) {
+      console.log(11111111111, i)
+      lines[i - 1] += lines[i]
+      lines.splice(i, 1)
+      console.log(lines)
+      continue
+    }
+    i += 1
+  }
+  console.log(lines)
+  return lines
+    .join("\n")
+    .replace(/\n\n/g, "\n")
+    .replace(/,\s*/g, ",\n")
+    .replace(/[ ]+/g, " ")
 }
 
 function normalize(text) {
   return text
-    .replace(/\./g, ".\n")
-    .replace(/\,/g, ",\n")
+    .replace(/\n/g, " ")
     .replace(/[ ]+/g, " ")
-    .replace(/\n+/g, "\n")
+    .replace(/\s*\./g, ".\n")
+    .replace(/\s*\,/g, ",\n")
     .replace(/"/g, "\n")
     .replace(/:/g, "\n")
     .replace(/^\s+/gm, "")
     .replace(/\s+$/gm, "")
     .trim()
+}
+
+function isTextLine(line) {
+  return /^\s*\w+/.test(line)
 }
