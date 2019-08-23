bf10acf7a403e42577fdaa92c0ff2ea36f482d22
Brian Dewey <bdewey@gmail.com>
2019-08-23 11:06:08 -0700

    Print out PR info for each branch in the stack

--- src/cmd/stack.go
+++ src/cmd/stack.go
@@ -1,11 +1,22 @@
 package cmd
 
 import (
-	"github.com/Originate/git-town/src/cfmt"
-	"github.com/Originate/git-town/src/git"
+	"unicode/utf8"
 
+	"github.com/Originate/git-town/src/cfmt"
+	"github.com/Originate/git-town/src/drivers"
+	"github.com/Originate/git-town/src/git"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+
+	"strings"
 )
+
+type branchInfo struct {
+	Branch    string
+	IsCurrent bool
+	PrInfo    string
+}
 
 var stackCmd = &cobra.Command{
 	Use:   "stack",
@@ -14,7 +25,7 @@ var stackCmd = &cobra.Command{
 
 Shows information about the current stack of changes.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		printStackInfo()
+		printBranchInfo(getBranchInfo())
 	},
 	Args: cobra.NoArgs,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
@@ -22,17 +33,61 @@ Shows information about the current stack of changes.`,
 	},
 }
 
-func printStackInfo() {
+func getBranchInfo() []branchInfo {
+	var info []branchInfo
 	currentBranch := git.GetCurrentBranchName()
 	for _, branch := range git.GetAncestorBranches(currentBranch) {
-		cfmt.Println(branch)
+		info = append(info, branchInfo{Branch: branch})
 	}
 	currentChildren := []string{currentBranch}
 	for ; len(currentChildren) == 1; currentChildren = git.GetChildBranches(currentChildren[0]) {
-		if currentChildren[0] == currentBranch {
+		isCurrent := currentChildren[0] == currentBranch
+		info = append(info, branchInfo{Branch: currentChildren[0], IsCurrent: isCurrent})
+	}
+
+	// Look for PR info for each of the branches.
+	driver := drivers.GetActiveDriver()
+	for i := range info {
+		if i > 0 {
+			canMerge, defaultCommitMessage, _ := driver.CanMergePullRequest(info[i].Branch, info[i-1].Branch)
+			if canMerge {
+				info[i].PrInfo = defaultCommitMessage
+			}
+		}
+	}
+	return info
+}
+
+func printBranchInfo(branchInfo []branchInfo) {
+	longestBranchRuneCount := 0
+	for _, info := range branchInfo {
+		currentRuneCount := utf8.RuneCountInString(info.Branch)
+		// The current branch needs to display "* " at the start
+		// TODO: Factor "* " into a constant where I can find its rune length
+		if info.IsCurrent {
+			currentRuneCount += 2
+		}
+		// Always allow for 2 spaces after the end of the string
+		currentRuneCount += 2
+		if currentRuneCount > longestBranchRuneCount {
+			longestBranchRuneCount = currentRuneCount
+		}
+	}
+	for _, info := range branchInfo {
+		if info.IsCurrent {
 			cfmt.Print("* ")
 		}
-		cfmt.Println(currentChildren[0])
+		branchColor := color.New(color.Bold)
+		if info.IsCurrent {
+			branchColor = branchColor.Add(color.FgCyan)
+		}
+		cfmt.Print(branchColor.Sprintf(info.Branch))
+		spacesToPrint := longestBranchRuneCount - utf8.RuneCountInString(info.Branch)
+		if info.IsCurrent {
+			spacesToPrint -= 2
+		}
+		cfmt.Print(strings.Repeat(" ", spacesToPrint))
+		cfmt.Println(info.Branch, info.PrInfo)
 	}
 }
 
